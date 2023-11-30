bbd5f55450deb01b17018cd76669ce149010de35
Alan <cortez.alan060@gmail.com>
2023-11-30 12:04:21 -0800

    tweak shift logic, finish reduce logic of parser

--- parse.py
+++ parse.py
@@ -62,8 +62,7 @@ def begin_parse(input):
     print(f'{step} | {stack} | {"".join(input)} | ')
     step += 1
 
-    while(step != 4):
-        # next_number = stack[-1];
+    while(step != 20):
         next_value = input.pop(0)
 
         table_result = check_parsing_table(stack[-1], next_value)
@@ -75,12 +74,42 @@ def begin_parse(input):
         elif(table_result[0] == "S"):
             print("shift")
             stack.append(next_value)
-            stack.append(int(table_result[1]))
+            stack.append(int(table_result[1:]))
         # If the result is to reduce then do this
         elif(table_result[0] == "R"):
             print("reduce")
+            
+            value_to_reduce = ""
+            temp_stack = []
+
+            while True:
+                temp_stack.insert(0, stack[-2])
+                value_to_reduce = ''.join(temp_stack)
+                # print(f'thingy: {value_to_reduce} temp stack: {temp_stack}')
+                stack.pop()
+                stack.pop()
+                if (value_to_reduce == "$"):
+                    return False
+                elif (value_to_reduce in reduction_rules[table_result]):
+                    reduced_letter = reduction_rules[table_result][value_to_reduce]
+
+                    # print(f'STACK NOW: {stack}')
+                    # print(f'LETTER TO REDUCE: {reduced_letter}')
+                    # print(f'GOTO NUMBER: {stack[-1]}')
+
+                    goto_number = check_parsing_table(stack[-1], reduced_letter)
+                    
+                    stack.append(reduced_letter)
+                    stack.append(goto_number)
+                    # make sure we add back the old value to the input stack because we cant get rid of it yet
+                    input.insert(0, next_value)
+                    break
+                
+
+
         # If neither then just cry honestly
         else:
+            print("actually false")
             return False
 
         
@@ -88,4 +117,8 @@ def begin_parse(input):
         step += 1
 
 
-begin_parse("(id+id)*id$")
+if begin_parse("(id+id)*id$"):
+    print("VALID")
+else:
+    print("NOT VALID")
+
